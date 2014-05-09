e60a7523efb032316c83c341d38dda278ead2032
Sterlin <samsterlin@gmail.com>
2014-05-09 10:35:54 +0530

    Update example.php
    Added the method - Search for the value by the field and condition

--- example.php
+++ example.php
@@ -4,9 +4,18 @@ include ("lib/awsCloudSearch.php");
 
 // Add your search domain and domain id
 $aws = new awsCloudSearch(DOMAIN_NAME, DOMAIN_ID);
-
+//Set param values
+$myParam = array(
+		"size" => 2000,
+		"start" => 0,
+		"return-fields" => 'field1,field2,field3,field4,field5,field6',
+		//"rank" => '-docid'
+);
 // search for the value you are looking for
-$res = $aws->search("term");
+$res = $aws->search("term", $myParam);
+// Search for the value by the field and condition
+/*$query = "(and field1:'value' field2:'value')";
+$res = $aws->searchby_field($query, $myParam);*/
 
 // if the HTTP_CODE is 200 you have a winner
 if ($aws->http_code == 200)
